fe6e3a0cc46cdc8efaeb0ed93af3f7a1a35e8508
V <OINV5657537@saint-gobain.com>
2021-09-05 21:20:40 +0530

    Add table

---
+++ Migrations/1.1.0-Changes/002_20210905-2120_OINV5657537.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="a5f05562-7a3d-450e-ae9e-0acd26da54a9" />
+GO
+
+PRINT N'Creating [dbo].[Employee1]'
+GO
+CREATE TABLE [dbo].[Employee1]
+(
+[PersonID] [int] NULL,
+[LastName] [varchar] (255) NULL,
+[FirstName] [varchar] (255) NULL,
+[Address] [varchar] (255) NULL,
+[City] [varchar] (255) NULL
+)
+GO
